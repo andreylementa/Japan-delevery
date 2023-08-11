cae1871c343803b69ae038f097fafa6b1b1ac869
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-08-11 12:58:40 +0300

    Отображение блюд

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header/Header";
 import React from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </React.Fragment>
   );
 }
