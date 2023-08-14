4b2c27d0bd3d35876f6aa1d9cbf02b643c2a20cb
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-08-14 09:55:01 +0300

    добавление модального окна

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import style from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={style["cart-items"]}>
+      {[{ id: "m1", name: "sushi", amount: 2, price: 10.99 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={style.total}>
+        <div>Итого</div>
+        <div className="">49.99</div>
+      </div>
+      <div className={style.actions}>
+        <button className={style["button--alt"]}>Закрыть</button>
+        <button className={style.button}>Заказать</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
